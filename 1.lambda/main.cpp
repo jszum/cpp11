7afd8b367cf65efb7dfb5e9ae75dff8ca06dcd1e
jszum <jaroslaw.szumega@gmail.com>
2015-07-22 13:53:23 +0200

    Lambda with capture

--- 1.lambda/main.cpp
+++ 1.lambda/main.cpp
@@ -4,10 +4,20 @@
 int main() {
 
     auto lambda = [](){return 5;};
-    int tab[] = {9,8,7,6,5,13,4,3,2,1};
+    std::cout<<"using lambda(ret 5) = "<<lambda();
 
+
+    int tab[] = {9,8,7,6,5,13,4,3,2,1};
     std::sort(std::begin(tab), std::end(tab), [](int i, int j){return i > j;});
     std::for_each(std::begin(tab), std::end(tab), [](int n) { std::cout<<n<<" ";});
+    std::cout<<std::endl;
+
+    int number = 42;
+
+    auto capture_lambda = [&number](int n){return number+n;};
+    std::cout<<capture_lambda(0)<<std::endl;
+    std::cout<<capture_lambda(1)<<std::endl;
+
 
     return 0;
 }
